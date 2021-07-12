1bd105e2f024dbcd24152ce95d906ab44ce3a4a6
bangjinhyuk <jinhyuk0103@gmail.com>
2021-07-12 19:01:07 +0900

    spring boot start & GET Method exam

---
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -0,0 +1,70 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.userRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/get")
+public class GetApiController {
+
+    @GetMapping(path = "/hello") //최신 방법
+    public String hello(){
+        return "hello spring boot";
+    }
+
+    @RequestMapping(path = "/hi",method = RequestMethod.GET) //오래전 방법
+    public String hi(){
+        return "hi spring boot";
+    }
+
+    @GetMapping("path-variable/{name}")
+    public String pathVariable(@PathVariable String name){
+        System.out.println("pathVariable :"+name);
+        return name;
+    }
+
+    @GetMapping("path-variables/{id}") //이름 일치 시켜주는법
+    public String pathVariables(@PathVariable(name = "id") String pathName){
+        System.out.println("pathVariable :"+pathName);
+        return pathName;
+    }
+
+    @GetMapping(path = "/query-param") //뭘 받는지 모름 암거나 받아오는거
+    public String queryParam(@RequestParam Map <String,String> queryParam) {
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping(path = "/query-param2") //따로따로
+    public String queryParam2(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + email+ age;
+    }
+
+    @GetMapping(path = "/query-param3") //객페로 받아오기
+    public String queryParam3(userRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
+}
