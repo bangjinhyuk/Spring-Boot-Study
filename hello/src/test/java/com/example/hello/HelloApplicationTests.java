6416012e4494c117d6c10663325c2c7bab379bcc
bangjinhyuk <jinhyuk0103@gmail.com>
2021-07-12 23:33:49 +0900

    Object Mapper exam

--- hello/src/test/java/com/example/hello/HelloApplicationTests.java
+++ hello/src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.hello;
 
+import com.example.hello.dto.userRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("-------");
+        //Text JSON -> Object
+        //Object -> Text JSON
+
+        //controller req json(text) -> object
+        //res object -> json(text)
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        //Object -> Text JSON
+        userRequest user = new userRequest();
+        user.setAge(19);
+        user.setName("123");
+        String text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        //Text JSON -> Object
+        user = objectMapper.readValue(text,userRequest.class);
+        System.out.println(user);
+
     }
 
 }
