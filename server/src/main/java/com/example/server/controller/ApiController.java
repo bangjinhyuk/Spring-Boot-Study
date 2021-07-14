89dece9487bc62b9e2315221f293b14eb5f7ae34
bangjinhyuk <jinhyuk0103@gmail.com>
2021-07-15 03:35:01 +0900

    Naver Api exam

--- server/src/main/java/com/example/server/controller/ApiController.java
+++ server/src/main/java/com/example/server/controller/ApiController.java
@@ -4,7 +4,17 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
@@ -52,5 +62,34 @@ public class ApiController {
         );
         response.setRbody(user.getRbody());
         return  response;
+    }
+    @GetMapping("/naver")
+    public String naver(){
+
+        String query = "아주대";
+        String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", query)
+                .queryParam("display",10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode()
+                .build()
+                .toUri();
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","mrVrgSGIpBkVPs7HwJ2t")
+                .header("X-Naver-Client-Secret","yxEhqQw1aS")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req,String.class);
+
+        return result.getBody();
     }
 }
