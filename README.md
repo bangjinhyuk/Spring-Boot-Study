4d18c6db3166710779f1577ecdecd02f25427d33
bangjinhyuk <jinhyuk0103@gmail.com>
2021-07-24 20:14:03 +0900

    Entity cache Study

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Spring-Boot-Study
 
+### Entity cache flush 발생 조건
+ - flush함수를 호출 했을때
+ - 한 transaction이 끝났을때 
+ - JPQL가 실행될때
+
 참고 자료
 
 [DAO, DTO, Entity Class 개념](https://gmlwjd9405.github.io/2018/12/25/difference-dao-dto-entity.html)
--- bookmanager/src/test/java/com/example/bookmanager/service/EntityManagerTest.java
+++ bookmanager/src/test/java/com/example/bookmanager/service/EntityManagerTest.java
@@ -1,11 +1,58 @@
 package com.example.bookmanager.service;
 
+import com.example.bookmanager.domain.User;
+import com.example.bookmanager.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+import javax.transaction.Transactional;
 
 /**
  * Created by bangjinhyuk on 2021/07/23.
  */
-@SpringBootTest
-public class EntityManagerTest {
 
+
+@SpringBootTest
+@Transactional
+public class EntityManagerTest {
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private UserRepository userRepository;
+    @Test
+    void entityManagerTest(){
+        System.out.println(entityManager.createQuery("select u from User u").getResultList());
+    }
+    @Test
+    void cacheFindTest(){
+        System.out.println(userRepository.findByEmail("leo@slowcampus.com"));
+        System.out.println(userRepository.findByEmail("leo@slowcampus.com"));
+        System.out.println(userRepository.findByEmail("leo@slowcampus.com"));
+    }
+
+    @Test
+    void cacheFindTest2(){
+
+        User user = userRepository.findById(1L).get();
+        user.setName("marrrrrrtin");
+
+        userRepository.save(user);
+
+//        userRepository.flush();
+
+        user.setEmail("marrrrrrrrtin@email");
+
+        userRepository.save(user);
+
+        userRepository.flush();
+        System.out.println(">>>>>>>1" + userRepository.findByEmail("martin@fastcampus.com"));
+
+        userRepository.flush();
+
+        System.out.println(">>>>>>>2" + userRepository.findByEmail("martin@fastcampus.com"));
+
+    }
 }
