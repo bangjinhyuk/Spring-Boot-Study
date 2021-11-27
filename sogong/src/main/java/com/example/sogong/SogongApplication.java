da8b6d97a2f039037b489eb9a6dea83c4a1b913c
bangjinhyuk <jinhyuk0103@gmail.com>
2021-11-27 17:48:06 +0900

    Create mini-project

---
+++ sogong/src/main/java/com/example/sogong/SogongApplication.java
@@ -0,0 +1,13 @@
+package com.example.sogong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SogongApplication.class, args);
+    }
+
+}
