d0383f51f79fd52f4ac594b7b11f2bdbb8722816
bangjinhyuk <jinhyuk0103@gmail.com>
2021-11-28 20:05:01 +0900

    feat:: User Entity list 초기화

--- sogong/src/main/java/com/example/sogong/domain/user/User.java
+++ sogong/src/main/java/com/example/sogong/domain/user/User.java
@@ -2,10 +2,13 @@ package com.example.sogong.domain.user;
 
 import com.example.sogong.domain.BaseTimeEntity;
 import com.example.sogong.domain.room.UserAndRoom;
+import com.example.sogong.dto.user.request.ModifyProfile;
 import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,38 +24,35 @@ public class User extends BaseTimeEntity {
     private long id;
 
     @NotBlank
-    private String login_id;
+    private String loginId;
 
     @NotBlank
     private String password;
 
-    @NotBlank
     private String name;
 
-    @NotBlank
     private String studentId;
 
-    @NotBlank
     private String major;
 
-    @NotBlank
+    @NotNull
     private MannerTempPrivate mannerTempPrivate;
 
     @OneToMany(mappedBy = "specUserId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @ToString.Exclude
-    private List<Spec> specList;
+    private List<Spec> specList = new ArrayList<>();
 
     @OneToMany(mappedBy = "mannerUserId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @ToString.Exclude
-    private List<Manner> mannerList;
+    private List<Manner> mannerList = new ArrayList<>();
 
     @OneToMany(mappedBy = "userAndRoomUserId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @ToString.Exclude
-    private List<UserAndRoom> userAndRoomList;
+    private List<UserAndRoom> userAndRoomList = new ArrayList<>();;
 
     @Builder
-    public User(String login_id, String password, String name, String studentId, String major, MannerTempPrivate mannerTempPrivate, List<Spec> specList, List<Manner> mannerList, List<UserAndRoom> userAndRoomList) {
-        this.login_id = login_id;
+    public User(String loginId, String password, String name, String studentId, String major, MannerTempPrivate mannerTempPrivate, List<Spec> specList, List<Manner> mannerList, List<UserAndRoom> userAndRoomList) {
+        this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.studentId = studentId;
@@ -62,4 +62,11 @@ public class User extends BaseTimeEntity {
         this.mannerList = mannerList;
         this.userAndRoomList = userAndRoomList;
     }
+
+    public void modifyUser(ModifyProfile modifyProfile){
+        this.name = modifyProfile.getName();
+        this.major = modifyProfile.getMajor();
+        this.studentId = modifyProfile.getStudentId();
+    }
+
 }
